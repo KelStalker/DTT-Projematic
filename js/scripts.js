c0cc888a6d82a8faaf23c9aef1d09836acbf9557
Zohairajweh <zohair.ajweh@hotmail.com>
2020-03-21 18:15:57 -0700

    Add java Script file

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+var addTaskBtn = document.querySelector('#addTask');
+var myHeadingText=`our data type test`
+document.body.innerHTML+=
+`<section class="card-Preproject">
+<h2> Preproject </h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+</p>  <a href="" class="button">Learn more</a>
+<a> Download </a>
+
+
+`+myHeadingText+`
+</section>`;
+var headingTest =myHeadingText
+var addTaskBtn=0;
+if (addTaskBtn == 0) {
+    catagoryName.innerHTML = "The Chosen Category Is Premier League Football Teams";
+  } else if (chosenCategory === categories[1]) {
+    catagoryName.innerHTML = "The Chosen Category Is Films";
+  } else if (chosenCategory === categories[2]) {
+    catagoryName.innerHTML = "The Chosen Category Is Cities";
+  }
+
+
+
+addTaskBtn.addEventListener("click", myHeadingText);
