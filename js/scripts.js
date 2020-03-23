fe6c6b9dbc46e518eae3af325b9742ef1e374cf1
Zohairajweh <zohair.ajweh@hotmail.com>
2020-03-22 20:05:38 -0700

    creat hidden boxs

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ myHeading.addEventListener('mouseover',function(evnt){
 
     document.getElementById('Initiation').onclick = function(){
       document.body.innerHTML+=
-      `      <div id="inintionGrid">
+      `      <div class="inintionGrid">
       <div id="project-charter"><a href=""> project-charter</a></div>
       <div id="stakeholder-managementplan"><a href=""> stake holder management plan</a></div>
       <!-- <div id="stakeholder-register"><a href=""> stakeholder-register</a></div> -->
@@ -30,43 +30,10 @@ myHeading.addEventListener('mouseover',function(evnt){
     alert("zohair")
   } 
 }
-document.getElementById('Planning').onclick = function(){
-  document.body.innerHTML+=
-  `      <div id="planingGrid">
+// document.getElementById('Planning').onclick = function(){
+//   document.body.innerHTML+=
+//   `      <div class="planingGrid">
 
-  <div id="Project-managemnet-plan"><a href="">Project managemnet plan</a></div>
-  <div id="Scope-managemnet-plan"><a href="#"> Scope managemnet plan </a></div>
-  <div id="Cost-managemnet-plan"><a href="#"> Cost managemnet plan </a></div>
-  <div id="Schdule-managemnet-plan"><a href="#">Schdule managemnet plan</a></div>
-  <div id="Risk-managemnet-plan"><a href="#">Risk managemnet plan</a></div>
-  <div id="Communitcuion-managemnet-plan"><a href="#">Communitcuion managemnet plan </a></div>
-  <div id="resource-managemnet-plan"><a href="#">Resource managemnet plan </a></div>
-  <div id="procurement-managemnet-plan"><a href="#">procurement managemnet plan </a></div>
-  <div id="Quality-managemnet-plan"><a href="#">Quality managemnet plan </a></div>
-  <div id="Test-managemnet-plan"><a href="#">Test managemnet plan </a></div>
-  <div id="change-managemnet-plan"><a href="#">change managemnet plan </a></div>
-  <div id="Implementation-managemnet-plan"><a href="#">Implementation managemnet plan </a></div>
-  <div id="Traning-managemnet-plan"><a href="#">Traning managemnet plan </a></div>
-     <a href="#" id="hide-inintionGrid" onclick="hideFunction()">Hide</a>
-</div>   
-`;
-
-} 
-
- function hideFunction() {
-  var x = document.getElementById("planingGrid");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-// function ShowFunction() {
-//   var x = document.getElementById("hidew");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//     document.body.innerHTML=
-//   ` <div id="planingGrid">
 //   <div id="Project-managemnet-plan"><a href="">Project managemnet plan</a></div>
 //   <div id="Scope-managemnet-plan"><a href="#"> Scope managemnet plan </a></div>
 //   <div id="Cost-managemnet-plan"><a href="#"> Cost managemnet plan </a></div>
@@ -83,8 +50,25 @@ document.getElementById('Planning').onclick = function(){
 //      <a href="#" id="hide-inintionGrid" onclick="hideFunction()">Hide</a>
 // </div>   
 // `;
-    
+
+// } 
+
+//  function hideFunction() {
+//   var x = document.getElementsByClassName("planingGrid");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
 //   } else {
 //     x.style.display = "none";
 //   }
 // }
+function ShowFunction() {
+  var x = document.getElementById("planingGrid");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+   
+    
+  } else {
+    x.style.display = "none";
+  }
+}
+    
