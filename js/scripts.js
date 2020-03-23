db369558044a2532a5e0e498823a55f78b792a51
Zohairajweh <zohair.ajweh@hotmail.com>
2020-03-22 17:21:34 -0700

    edit Planing flow chart

--- js/scripts.js
+++ js/scripts.js
@@ -47,10 +47,44 @@ document.getElementById('Planning').onclick = function(){
   <div id="change-managemnet-plan"><a href="#">change managemnet plan </a></div>
   <div id="Implementation-managemnet-plan"><a href="#">Implementation managemnet plan </a></div>
   <div id="Traning-managemnet-plan"><a href="#">Traning managemnet plan </a></div>
+     <a href="#" id="hide-inintionGrid" onclick="hideFunction()">Hide</a>
 </div>   
 `;
-document.getElementById('hide-1').onclick = function(){
-alert("zohair")
+
 } 
+
+ function hideFunction() {
+  var x = document.getElementById("planingGrid");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
- 
+// function ShowFunction() {
+//   var x = document.getElementById("hidew");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//     document.body.innerHTML=
+//   ` <div id="planingGrid">
+//   <div id="Project-managemnet-plan"><a href="">Project managemnet plan</a></div>
+//   <div id="Scope-managemnet-plan"><a href="#"> Scope managemnet plan </a></div>
+//   <div id="Cost-managemnet-plan"><a href="#"> Cost managemnet plan </a></div>
+//   <div id="Schdule-managemnet-plan"><a href="#">Schdule managemnet plan</a></div>
+//   <div id="Risk-managemnet-plan"><a href="#">Risk managemnet plan</a></div>
+//   <div id="Communitcuion-managemnet-plan"><a href="#">Communitcuion managemnet plan </a></div>
+//   <div id="resource-managemnet-plan"><a href="#">Resource managemnet plan </a></div>
+//   <div id="procurement-managemnet-plan"><a href="#">procurement managemnet plan </a></div>
+//   <div id="Quality-managemnet-plan"><a href="#">Quality managemnet plan </a></div>
+//   <div id="Test-managemnet-plan"><a href="#">Test managemnet plan </a></div>
+//   <div id="change-managemnet-plan"><a href="#">change managemnet plan </a></div>
+//   <div id="Implementation-managemnet-plan"><a href="#">Implementation managemnet plan </a></div>
+//   <div id="Traning-managemnet-plan"><a href="#">Traning managemnet plan </a></div>
+//      <a href="#" id="hide-inintionGrid" onclick="hideFunction()">Hide</a>
+// </div>   
+// `;
+    
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
