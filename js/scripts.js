4eb03e860dbf014b10e8aede36133aaf32580b2f
Karan Pandher <karanpandher@live.ca>
2020-03-24 13:14:49 -0600

    delete this file

--- js/scripts.js
+++ js/scripts.js
@@ -84,6 +84,17 @@
 //   this.className= 'planingGrid';
 //   });
 
+function ShowFunctionLogin(){
+  var x = document.getElementById("nav-3-function");
+  
+  if (x.style.display === "none") {
+    x.style.display = "grid";
+   
+    
+  } else {
+    x.style.display = "none";
+  }
+
 function ShowFunctionPreproject(){
  
   var x = document.getElementById("pre-project-function");
@@ -165,6 +176,4 @@ function ShowFunctioncomare()
 function load(url)
 {
 window.location.href=url
-}
-
-
+} 
