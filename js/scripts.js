c4278bcd314c4c084ab6a98e8043829b8e01781d
Zohairajweh <zohair.ajweh@hotmail.com>
2020-03-25 19:06:24 -0700

    workink version Wednesday

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,4 @@
-// document.getElementById('Preproject').onclick = function(){
-//     document.body.innerHTML+=
-//     `<section class="card-Preproject">
-//     <h2> Preproject </h2>
-//     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-//     orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-//     orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-//     orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
-//     </p>  <a href="" class="button">Learn more</a>
-//     <a> Download </a>
-//     </section>`;
-// } 
-// var myHeading=document.getElementById('Preproject');
-// myHeading.addEventListener('mouseover',function(evnt){
-//   document.body.title="Lorem Ipsum is simply dumook a galley of type and scrambled it to make a type specimen book."
-//     });
 
-//     document.getElementById('Initiation').onclick = function(){
-//       document.body.innerHTML+=
-//       `      <div class="inintionGrid">
-//       <div id="project-charter"><a href=""> project-charter</a></div>
-//       <div id="stakeholder-managementplan"><a href=""> stake holder management plan</a></div>
-//       <!-- <div id="stakeholder-register"><a href=""> stakeholder-register</a></div> -->
-//       <a href="#" id="hide-inintionGrid">Hide</a>
-//   </div>      
-//    `;
-//   document.getElementById('hide-1').onclick = function(){
-//     alert("zohair")
-//   } 
-
-// document.getElementById('Planning').onclick = function(){
-//   document.body.innerHTML+=
-//   `      <div class="planingGrid">
-
-//   <div id="Project-managemnet-plan"><a href="">Project managemnet plan</a></div>
-//   <div id="Scope-managemnet-plan"><a href="#"> Scope managemnet plan </a></div>
-//   <div id="Cost-managemnet-plan"><a href="#"> Cost managemnet plan </a></div>
-//   <div id="Schdule-managemnet-plan"><a href="#">Schdule managemnet plan</a></div>
-//   <div id="Risk-managemnet-plan"><a href="#">Risk managemnet plan</a></div>
-//   <div id="Communitcuion-managemnet-plan"><a href="#">Communitcuion managemnet plan </a></div>
-//   <div id="resource-managemnet-plan"><a href="#">Resource managemnet plan </a></div>
-//   <div id="procurement-managemnet-plan"><a href="#">procurement managemnet plan </a></div>
-//   <div id="Quality-managemnet-plan"><a href="#">Quality managemnet plan </a></div>
-//   <div id="Test-managemnet-plan"><a href="#">Test managemnet plan </a></div>
-//   <div id="change-managemnet-plan"><a href="#">change managemnet plan </a></div>
-//   <div id="Implementation-managemnet-plan"><a href="#">Implementation managemnet plan </a></div>
-//   <div id="Traning-managemnet-plan"><a href="#">Traning managemnet plan </a></div>
-//      <a href="#" id="hide-inintionGrid" onclick="hideFunction()">Hide</a>
-// </div>   
-// `;
-
-// } 
-
-//  function hideFunction() {
-//   var x = document.getElementsByClassName("planingGrid");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-// function ShowFunctionPlanning() {
-//   var x = document.getElementById("planingGrid");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-   
-    
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-// function getStyleSheet(unique_title) {
-//   for(var i=0; i<document.styleSheets.length; i++) {
-//     var sheet = document.styleSheets[i];
-//     if(sheet.title == unique_title) {
-//       return sheet;
-//     }
-//   }
-// }
-// mouseOver.addEventListener('mouseover',function(evnt){
-//   this.className= 'planingGrid';
-//   });
 
 function ShowFunctionLogin(){
   var x = document.getElementById("nav-3-function");
@@ -163,9 +82,9 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "zz" && password == "zz"){
+if ( username == "dtt" && password == "123"){
 alert ("Login successfully");
-// window.location = "success.html"; // Redirecting to other page.
+// popup.classList.toggle("show");
 var loginText=document.getElementById("logIn");
 var loginForm=document.getElementById("login-id");
 var  stakeholderRegister=document.getElementById("stakeholder-register");
@@ -181,6 +100,9 @@ var testscript=document.getElementById("test-script");
 var satisfactionAssessment=document.getElementById("satisfaction-Assessment");
 var LessonsLearned=document.getElementById("Lessons-Learned");
 var statmnetofwork=document.getElementById("statmnet-of-work");
+var initiationpopup=document.getElementById("initiationpopup");
+
+
 loginForm.style.display ="none";
 loginText.textContent="signout";
 loginText.addEventListener('click',function(evnt){
@@ -200,6 +122,7 @@ if ( loginText.textContent=="signout"){
   satisfactionAssessment.style.display="block";
   LessonsLearned.style.display="block";
   statmnetofwork.style.display="block";
+  initiationpopup.style.display="none";
 }
 return false;
 }
@@ -216,27 +139,6 @@ return false;
 }
 }
  
-
-
-// function ShowFunctioncompare()
-// {
-//   var psw1 = document.getElementById("psw");
-//   var uname1 = document.getElementById("uname");
-//   var userName=uname1;
-//   var passwoed=psw1;
-//   var userName="zohair";
-//   var passwoed="1234"
-//     if(userName== passwoed){
-// alert (sss);    
-
-
-// }
-//     else
-
-//     {
-//       load("login.html");
-//     }
-// }
 function reloadThePage(){
   window.location.reload();
 } 
@@ -295,23 +197,25 @@ var statmnetofwork=document.getElementById("statmnet-of-work");
 function ShowFunctionlogin(){
   var x = document.getElementById("login-id");
   
-  if (x.style.display === "none") {
-    x.style.display = "grid";
+  if (x.style.display === "grid") {
+    x.style.display = "none";
    
     
   } else {
-    x.style.display = "none";
+    x.style.display = "grid";
   }
+
 }
 
-function myFunction() {
+
+function myPopupFunction() {
   var popup = document.getElementById("myPopup");
-  
+
   popup.classList.toggle("show");
 }
 
-function myclickFunction() {
-  var popup = document.getElementById("myPopup");
+// function myclickFunction() {
+//   var popup = document.getElementById("myPopup");
   
-alert();
-}
+// alert();
+// }
